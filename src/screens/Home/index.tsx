8b09a30d3be330eaa84d2cd0b3c3b02aa0829879
Alexandre <eztalenda@gmail.com>
2024-06-26 18:43:47 -0300

    feat: Add the touchableOpacity component in main project code.

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,13 +1,21 @@
-import { Text, View, TextInput } from 'react-native'
+import { Text, View, TextInput, TouchableOpacity } from 'react-native'
 import { styles } from './styles'
 
 export default function Home() {
+  const handleParticipantAdd = () => {
+    console.log('voce clicou no botao de adicionar!')
+  }
+
   return (
     <View style={styles.container}>
       <Text style={styles.eventName}>Nome do Evento</Text>
       <Text style={styles.eventDate}>Sexta, 4 de Novembro de 2024</Text>
 
       <TextInput style={styles.input} placeholder="Nome do Participante" placeholderTextColor={'#6b6b'} />
+
+      <TouchableOpacity style={styles.button} onPress={handleParticipantAdd}>
+        <Text style={styles.buttonText}>+</Text>
+      </TouchableOpacity>
     </View>
   )
 }
